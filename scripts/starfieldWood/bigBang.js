4657b4940c794fe2b6180c895be56980e84d95f1
WoodBPatrick <43044096+WoodBPatrick@users.noreply.github.com>
2019-11-21 15:13:04 -0600

    Add files via upload

--- scripts/starfieldWood/bigBang.js
+++ scripts/starfieldWood/bigBang.js
@@ -3,13 +3,13 @@ class bigBang {
     this.x=x;
     this.y=y;
     this.speed=.7;
-    this.ang=(Math.PI*2);
+    this.ang=(PI*2);
     this.sizeX=0;
     this.sizeY=0;
   }
   move() {
     this.sizeX+=this.ang*this.speed;
-    this.sizeY+=this.ang*thi.speed;
+    this.sizeY+=this.ang*this.speed;
   }
   restartBang(){
     this.sizeX-=this.ang*this.speed;
@@ -18,7 +18,7 @@ class bigBang {
   show() {
     fill(0);
     stroke(255);
-    ellipse(this.x, this.y, Math.floor(this.sizeX), Math.floor(this.sizeY));
+    ellipse(this.x, this.y, floor(this.sizeX), floor(this.sizeY));
   }
   getSizeX(){
     return this.sizeX;
--- scripts/starfieldWood/starfieldRunner.js
+++ scripts/starfieldWood/starfieldRunner.js
@@ -1,28 +1,43 @@
-var particles;
+var particles,bang,odd,jumbo;
 var restarting=false;
 function setup() {
-  createCanvas(800,800);
+  createCanvas(1200,800);
   particles=[];
-  var bang = new bigBang(width/2, height/2);
-  var odd = new OddballParticle(width/2, height/2);
-  var jumbo = new JumboParticle(width/2, height/2);
+  bang = new bigBang(width/2, height/2);
+  odd = new OddballParticle(width/2, height/2);
+  jumbo = new JumboParticle(width/2, height/2);
   for (var i=0; i<800; i++) {
     particles[i] = new normalParticle(width/2, height/2);
   }
 }
 function draw() {
   background(255);
-  for (var i=0; i<particles.length; i++) {
-    if (!restarting) {
-      particles[i].move();
-      particles[i].show();
-      if (bang.getSizeX() > 4000)
-        restarting = true;
-    } else if (restarting) {
-      particles[i].restartBang();
-      particles[i].show();
-      if (particles[0].getSizeX() < 0)
-        restarting = false;
-    }
+  if(!restarting){
+      bang.move();
+      bang.show();
+      jumbo.move();
+      jumbo.show();
+      odd.move();
+      odd.show();
+      for(var i=0;i<particles.length;i++){
+          particles[i].move();
+          particles[i].show();
+      }
+      if(bang.getSizeX()>4000)
+        restarting=true;
   }
+    else if(restarting){
+        bang.restartBang();
+        bang.show();
+        jumbo.restartBang();
+        jumbo.show();
+        odd.restartBang();
+        odd.show();
+        for(var i=0;i<particles.length;i++){
+          particles[i].restartBang();
+          particles[i].show();
+        }
+        if(bang.getSizeX()<0)
+            restarting=false;
+    }
 }
